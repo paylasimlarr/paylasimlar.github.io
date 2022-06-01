09256b04be708f488ecd88b39b6dd7ebbec78e58
paylasimlarr <106611418+paylasimlarr@users.noreply.github.com>
2022-06-01 23:39:12 +0300

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBFALlD6U_I46_y2I8aNGYJZDy5fW9orlA",
+  authDomain: "register-1-5793f.firebaseapp.com",
+  databaseURL: "https://register-1-5793f-default-rtdb.firebaseio.com",
+  projectId: "register-1-5793f",
+  storageBucket: "register-1-5793f.appspot.com",
+  messagingSenderId: "298981607200",
+  appId: "1:298981607200:web:25a7b0ad9a8e9de0bbb945"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
